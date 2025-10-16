398da02ccd8bd2e59cb0675e0696d2f4dbc7073c
verydd <127667965+verydd@users.noreply.github.com>
2025-10-16 21:54:42 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "帮帮音乐";
     }
 
     function updateMobileOverlayScrim() {
